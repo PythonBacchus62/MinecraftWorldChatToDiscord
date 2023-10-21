331610f30d390231de43010c4f28726ddffea60d
Python-Legend <148625753+Python-Legend@users.noreply.github.com>
2023-10-21 15:06:54 +0200

    >
    Auto Start Datei fehlt noch, kommt aber noch

---
+++ AutoStart.ps1
@@ -0,0 +1,5 @@
+sleep 10
+
+Get-Process -Name Minecraft.Windows
+Start-Process 
+
---
+++ MinecraftChatToTXT.py
@@ -0,0 +1,83 @@
+from PIL import Image 
+from pytesseract import pytesseract 
+import pyscreenshot
+import pyautogui
+from time import sleep
+
+Loop = 0
+Chat = 0
+
+while Loop == 0:
+    Chat = 1
+    if Chat == 1:
+        image_demo = pyscreenshot.grab(bbox=(0, 885, 1895, 935))
+        image_demo.save("C:\Minecraft\Chat.png")
+
+
+        path_to_tesseract = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+        image_path = "C:\Minecraft\Chat.png"
+
+        # Opening the image & storing it in an image object 
+        img = Image.open(image_path) 
+
+        # Providing the tesseract executable 
+        # location to pytesseract library 
+        pytesseract.tesseract_cmd = path_to_tesseract 
+
+        # Passing the image object to image_to_string() function 
+        # This function will extract the text from the image 
+        text = pytesseract.image_to_string(img) 
+
+        # Displaying the extracted text 
+        print(text)
+        try:
+            if text[0] == "<":
+                OldMSG = open("C:\Minecraft\MinecraftDiscordLog.json", "r")
+                OldMSG = OldMSG.read()
+                if text == OldMSG:
+                    pass
+                else:
+                    print(text)
+                    file1 = open("C:\Minecraft\MinecraftDiscord.json", "a")
+                    file1.write(text)
+                    file1.close()
+                    file2 = open("C:\Minecraft\MinecraftDiscordLog.json", "w")
+                    file2.write(text)
+                    file2.close()
+
+
+            if text[1] == "+":
+                OldMSG = open("C:\Minecraft\MinecraftDiscordLog.json", "r")
+                OldMSG = OldMSG.read()
+                if text == OldMSG:
+                    pass
+                else:
+                    print(text)
+                    file1 = open("C:\Minecraft\MinecraftDiscord.json", "a")
+                    file1.write(text)
+                    file1.close()
+                    file2 = open("C:\Minecraft\MinecraftDiscordLog.json", "w")
+                    file2.write(text)
+                    file2.close()
+
+
+            if text[1] == "-":
+                OldMSG = open("C:\Minecraft\MinecraftDiscordLog.json", "r")
+                OldMSG = OldMSG.read()
+                if text == OldMSG:
+                    pass
+                else:
+                    print(text)
+                    file1 = open("C:\Minecraft\MinecraftDiscord.json", "a")
+                    file1.write(text)
+                    file1.close()
+                    file2 = open("C:\Minecraft\MinecraftDiscordLog.json", "w")
+                    file2.write(text)
+                    file2.close()
+        except:
+            pass
+    
+    
+
+    
+
